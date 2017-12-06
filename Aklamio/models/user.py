85b29066808856b96bba374092a1024728529e3d
YanivHamou <34278637+YanivHamou@users.noreply.github.com>
2017-12-06 10:19:24 +0100

    Update user.py
    Latest Version

--- Aklamio/models/user.py
+++ Aklamio/models/user.py
@@ -1,23 +1,21 @@
 
 class user :
 
-    def __init__(self,name):
+    def __init__(self,name,):
         self.name=name
         self.portfolio =portfolio()
+        self.minimumStocks = 3
 
     def updatePortfolioShares(share):
-        print(1)
         self.portfolio.shares.append(share)
 
-    def updatePortfolioBalance(amount,action):
-        print(1)
+    def updatePortfolioBalance(self,amount,action):
         if action=='Sub':
             self.portfolio.balance = self.portfolio.balance - amount
         if action=='Add':
             self.portfolio.balance = self.portfolio.balance + amount
 
-
 class portfolio :
     def __init__(self):
-        shares=[]
-        balance=0
+        self.shares=[]
+        self.balance=0
