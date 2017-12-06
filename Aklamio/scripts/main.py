020227efd4df441e1e052c769323b75b17859246
yaniv h <yaniv.hamou@gmail.com>
2017-12-06 10:06:07 +0100

    code addtion-  sell al stocks in last day

--- Aklamio/scripts/main.py
+++ Aklamio/scripts/main.py
@@ -43,6 +43,7 @@ for s in stockMarket.stocks:
 #0   2016-12-05   6.500    BUY     CBK.DE
 #1   2016-12-13   7.740   SELL     CBK.DE
 
+dayindex=0
 
 # Main Loop - Represent the Trading
 # Loop throw trading days
@@ -58,11 +59,14 @@ for s in stockMarket.stocks:
 #            - Update user shares
 #            - Update user balance
 for day in tradingCycle:
+     dayindex=dayindex+1 #
      for stck in stockMarket.stocks:
          # get only rows with data
          if (len(stck.bestTrades[(stck.bestTrades['Date'] == day)]>0)) :
              #convert to single row
              row= stck.bestTrades[(stck.bestTrades['Date'] == day)]
+
+             #BUY
              if row['Action'].item()=='BUY':
                  #calculate 10 shares value
                  amount=(row['Price'].item() * 10) + stockMarket.tradeValue
@@ -70,17 +74,22 @@ for day in tradingCycle:
                  #check if user have money to buy
                     user.portfolio.balance = user.portfolio.balance- amount
                     user.portfolio.shares.append(row)
+             # SELL
              if row['Action'].item() == 'SELL':
                  # check user not have at least 4 stocks - cant go lower then 3
                  if (len(user.portfolio.shares)>3):
                      # calculate 10 shares
                      amount = (row['Price'].item() * 10) + stockMarket.tradeValue
                      user.portfolio.balance = user.portfolio.balance + amount
-
+                     # Pop out the Stock from users shares
                      for i in range(len(user.portfolio.shares) - 1):
                          if (user.portfolio.shares[i]['Stock'].item()==row['Stock'].item()):
                             user.portfolio.shares.pop(i)
                          break
+       # sell al stocks in last day
+     if dayindex == len(tradingCycle):
+         while user.portfolio.shares:
+             user.portfolio.shares.pop()
 
 
 
