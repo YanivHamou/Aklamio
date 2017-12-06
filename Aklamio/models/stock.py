65f2f0862c84799d6672b1ffe0b7d439b5d6b5fa
YanivHamou <34278637+YanivHamou@users.noreply.github.com>
2017-12-06 10:18:45 +0100

    Update Stock.py
    Latest File version

--- Aklamio/models/stock.py
+++ Aklamio/models/stock.py
@@ -6,16 +6,18 @@ import errno
 import datetime
 
 
-class stock (object):
+class stock :
 
     def __init__(self,name):
         self.name=name
         self.symbol=name
 
         #load History data
-        stockHistory = stockdata(name)
-        self.history= stockHistory.getHistoricalData()
+        stockData = stockdata(name)
+        self.history= stockData.getHistoricalData()
 
+        #build trades recommendation based on history
+        self.bestTrades= stockData.suggestTradesPaths(self.history,self.name)
 
 class stockdata:
 
@@ -29,33 +31,66 @@ class stockdata:
             self.records.append(rec)
             self.n_records += 1
 
-        # Read Data from Csv files based on Company Name
+        # ///////////////////////////////////////////////////////////////////////////////////
+        #  @brief    Based on Company Name - Read Data from Csv files.
+        #  @details  have 3 parts:
+        #               Read Csv
+        #               Calculate MinimumLoss
+        #               Convert date field to DateTime Object
+        # //////////////////////////////////////////////////////////////////////////////
         def getHistoricalData(self):
 
-            df = pd.read_csv(utils.getfullpath(self.name), sep=',')
+            try:
+                df = pd.read_csv(utils.getfullpath(self.name), sep=',')
+            except:
+                raise KeyError (errno.ENOENT,'Error : create History model for Stock : read CSV :' + name)
 
             # New column represent the change in stock Price
             df['MinimumLoss']=df['Open'].diff()
-
-            #convert to Date Type
-            df['Date']=df['Date'].apply(lambda x: utils.convertDate(x,"%m/%d/%Y"))
-
-            #fill None type with zero
-            df.fillna(0,inplace=True)
-
             df.MinimumLoss = df.MinimumLoss.astype(float).fillna(0.0)
 
-            #df.to_csv('f.csv')
+            #convert to Date Type
+            df['Date']=df['Date'].apply(lambda x: utils.convertDate(x,"%Y-%m-%d"))
 
+            #df.to_csv('f.csv')
             return df
 
 
-            try:
-                print(x)
+        # ///////////////////////////////////////////////////////////////////////////////////
+        #  @brief    Based on Company data - Build recommendation trades.
+        #            Finds the optimal (or nearly optimal) order of trades to maximize profit
+        #            All this is accomplished through a simple day-to-day comparison
+        #  @details  Loop throw all The history:
+        #               If price decreases tomorrow:
+        #                   - if you suggest buy a share, sell.
+        #                   - if you don't, wait to buy
+        #                If price increases tomorrow:
+        #                   - if you suggest buy a share, buy.
+        #                   - if you do, wait to sell
+        #  @parameters :
+        #             stockHistory  - stock History data
+        #             name          - stock name
+        # //////////////////////////////////////////////////////////////////////////////
+        def suggestTradesPaths(self,stockHistory,name):
 
-            except:
-                raise KeyError (errno.ENOENT,'Error create History model for Stock :' + name)
-                return  self.records
+            trades = []  # List of trades
+            share = 0  # boolean
+            trades_x = []
 
+            for i in range(len(stockHistory) - 1):
 
+                if not share:  # Share not held
+                    if stockHistory.loc[i + 1]['Open'] > stockHistory.loc[i]['Open']:  # Increasing tomorrow
+                        trades.append([i])  # Buy and add new trade
+                        trades_x.append([stockHistory.loc[i]['Date'], stockHistory.loc[i]['Open'], 'BUY', name])
+                        share = 1
 
+                else:  # Share held
+                    if stockHistory.loc[i + 1]['Open'] < stockHistory.loc[i]['Open']:  # Decreasing tomorrow
+                        trades[len(trades) - 1].append(i)  # Add sell day to existing trade
+                        trades_x.append(
+                            [stockHistory.loc[i]['Date'], stockHistory.loc[i]['Open'], 'SELL', name])
+                        share = 0
+
+            df_trades = pd.DataFrame(trades_x, columns=['Date', 'Price', 'Action', 'Stock'])
+            return df_trades
